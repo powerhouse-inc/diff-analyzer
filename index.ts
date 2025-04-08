e448cb3482b648d3715b637f694b9e627f651074
Benjamin Jordan <benjamin@thegoldenmule.com>
2025-04-08 09:58:51 -0500

    updating processors

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-import * as documentModelsExports from './document-models';
-        import * as editorsExports from './editors';
+import * as documentModelsExports from "./document-models";
+import * as editorsExports from "./editors";
+import { processorFactory } from "processors";
 
-        export const documentModels = Object.values(documentModelsExports);
-        export const editors = Object.values(editorsExports);
+export const documentModels = Object.values(documentModelsExports);
+export const editors = Object.values(editorsExports);
+export const processors = processorFactory;
--- processors/index.ts
+++ processors/index.ts
@@ -1,1 +1,28 @@
-export * as DiffAnalyticsProcessor from "./diff-analytics/index.js";
+/**
+ * This is a scaffold file meant for customization.
+ * Delete the file and run the code generator again to have it reset
+ */
+
+import {
+  ProcessorFactory,
+  ReactorModule,
+  ProcessorRecord,
+} from "@powerhousedao/reactor-api";
+import { DiffAnalyticsProcessor } from "./diff-analytics/index.js";
+
+export const processorFactory: ProcessorFactory = (
+  driveId: string,
+  module: ReactorModule,
+): ProcessorRecord[] => {
+  return [
+    {
+      processor: new DiffAnalyticsProcessor(module.analyticsStore),
+      filter: {
+        branch: ["main"],
+        documentId: ["*"],
+        scope: ["*"],
+        documentType: ["*"],
+      },
+    },
+  ];
+};
